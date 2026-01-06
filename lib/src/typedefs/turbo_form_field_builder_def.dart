e77306d5f07bc14431bb73fabf1da4ebd517e933
appboypov <info@codaveto.com>
2026-01-06 21:20:24 +0100

    Update flutter_lints to ^6.0.0 and fix warnings

--- lib/src/typedefs/turbo_form_field_builder_def.dart
+++ lib/src/typedefs/turbo_form_field_builder_def.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/widgets.dart';
 import '../config/turbo_form_field_config.dart';
 
-typedef TurboFormFieldBuilderDef<T> = Widget Function(
-  BuildContext context,
-  TurboFormFieldConfig<T> fieldConfig,
-  Widget? child,
-);
+typedef TurboFormFieldBuilderDef<T> =
+    Widget Function(
+      BuildContext context,
+      TurboFormFieldConfig<T> fieldConfig,
+      Widget? child,
+    );
